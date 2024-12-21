f2fb3f381b98c20b2a11ca1cfe7ded21edc75800
Ram <ramr.f@yandex.ru>
2024-12-21 23:42:16 +0300

    2112finish

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hello');
+console.log('Hello from JS');
+let userName = 'Ram';
+const Pi = 3.16;
+userName = 'Ramil';
+console.log(Pi,userName);
+// don't use var
+/* basic arithmetic operations */
+
+let area = Pi * 5;
+console.log('division = ' + 8/2);
+console.log('pow = ' + 2**5);
+console.log(area);
+
+
+let age = 18 + 2;
+age += 2;
+age++;
+age--;
+
+// > < >= <= == ===
+console.log(age > Pi);
+
+//priority
+
+ 
